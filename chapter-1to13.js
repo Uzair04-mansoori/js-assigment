f28f5388d39af3bc6c99c17e3ab50475c9598891
Uzair04-mansoori <62034064+Uzair04-mansoori@users.noreply.github.com>
2026-04-03 10:35:08 +0500

    Chapter 1 to 13

---
+++ chapter-1to13.js
@@ -0,0 +1,97 @@
+//Chapter 1 Alert & Prompt
+//Answer 1
+//alert('');
+
+//Answer 2
+//alert('Error! Please enter a valid password.');
+
+//Answer 3
+//alert('Welcome to JS Land...\nHappy Coding');
+
+//Answer 4
+//alert('Welcome to JS Land...');
+
+//Answer 5
+//alert('Happy Coding\n');
+
+//Answer 6
+//var $text = prompt('');
+//console.log($text);
+
+//Answer 6 + 7
+// The Following answer is printed in html file since because of task 7
+
+// Chapter 2 Variable & String
+
+//Answer 1
+//var username;
+
+//Answer 2
+//var myName = 'Uzair';
+
+//Answer 3
+// var $message = "Hello World";
+// alert($message);
+
+//Answer 4
+// var $item = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+// alert($item);
+
+//Answer 5
+// var $email = 'uzairmansoori@gmail.com'
+// var $message = "My Email Address is " + $email;
+// alert($message);
+
+//Answer 6
+// var item1 = prompt('Enter name of item you want to buy');
+// var price1 = item1 ? prompt('Enter price of' + item1) : 0;
+
+// var item2 = prompt('Enter name of item you want to buy');
+// var price1 = item2 ? prompt('Enter price of' + item2) : 0;
+
+// price1 = parseFloat(price1) || 0;
+// price2 = parseFloat(price2) || 0;
+
+// var total = price1 + price2;
+
+// var disc = 0;
+
+// if(total <= 3000){
+//     disc = 0;
+// }
+// else if(total => 5000 && total < 10000){
+//     disc = total * 0.05;
+// }
+// else if(total => 10000){
+//     disc = total * 0.10;
+// }
+// else{
+//     console.log("Please buy something");
+// };
+
+// var final = total - disc;
+
+// document.writeIn{
+//     `
+//         <table border='' width='60%' align='center'>
+//             <tr>
+//                 <th>Item</th>
+//                 <th>Price</th>
+//             </tr>
+//             <tr>
+//                 <td>$(item1 || '')</td>
+//                 <td>$(price1 || '')</td>
+//                 <td>$total</td>
+//                 <td>$(disc.toFixed(2))</td>
+//                 <td>$(final.toFixed(2))</td>
+//             </tr>
+//             <tr>
+//                 <td>$(item1 || '')</td>
+//                 <td>$(price2 || '')</td>
+//                 <td>$total</td>
+//                 <td>$(disc.toFixed(2))</td>
+//                 <td>$(final.toFixed(2))</td>
+//             </tr>
+//         </table>
+//     `
+// }
